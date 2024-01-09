0f4746d04d5ec3dfb8767eead12782f7354cd081
Julian Mendez <julian.mendez@gmail.com>
2024-01-09 11:27:59 +0100

    Update methods that retrieve elements

--- market-core/src/main/scala/soda/se/umu/cs/soda/prototype/example/market/Package.scala
+++ market-core/src/main/scala/soda/se/umu/cs/soda/prototype/example/market/Package.scala
@@ -45,22 +45,26 @@ case class Market_ (accounts : List [Int], items : List [Item]) extends Market
 trait MarketMod
 {
 
-  def get [A] (list : List [A]) (index : Index) : A =
-    list.apply (index)
+  def get [A] (list : List [A]) (index : Index) : Option [A] =
+    list.lift (index)
 
 /*
   directive lean
-  def get {A : Type} (list : List (A)) (index : Index) : A :=
-    list.get! (index)
+  def get {A : Type} (list : List (A)) (index : Index) : Option (A) :=
+    list.get? (index)
 */
 
   def set [A] (list : List [A]) (index : Index) (element : A) : List [A] =
-    list.updated (index , element)
+    if (index < list.length)
+    then list.updated (index , element)
+    else list
 
 /*
   directive lean
   def set {A : Type} (list : List (A)) (index : Index) (element : A) : List (A) :=
-    list.set (index) (element)
+    if (index < list.length)
+    then list.set (index) (element)
+    else list
 */
 
   def mk_market (new_accounts : List [Int] ) (new_items : List [Item] ) : Market =
@@ -73,7 +77,10 @@ trait MarketMod
     Item_ (item .owner, item .price, true)
 
   private def _advertise (items : List [Item] ) (item_id : Index) : List [Item] =
-    set (items) (item_id) (_advertise_item (get (items) (item_id) ) )
+    (get (items) (item_id) ) match  {
+      case Some (item) => set (items) (item_id) (_advertise_item (item) )
+      case otherwise => items
+    }
 
   def advertise (market : Market) (item_id : Index) : Market =
     mk_market (market .accounts) (_advertise (market .items) (item_id) )
@@ -82,16 +89,33 @@ trait MarketMod
     Item_ (item .owner, item .price, false)
 
   private def _remove_ad (items : List [Item] ) (item_id : Index) : List [Item] =
-    set (items) (item_id) (_hide_item (get (items) (item_id) ) )
+    (get (items) (item_id) ) match  {
+      case Some (item) => set (items) (item_id) (_hide_item (item) )
+      case otherwise => items
+    }
 
   def remove_ad (market : Market) (item_id : Index) : Market =
     mk_market (market .accounts) (_remove_ad (market .items) (item_id) )
 
+  private def _transfer_with_balances (accounts : List [Int] ) (origin : Index) (target : Index)
+      (amount : Int) (origin_balance : Int) (target_balance : Int) : List [Int] =
+    set (set (accounts) (origin) (origin_balance - amount) ) (target) (target_balance + amount)
+
+  private def _transfer_with (accounts : List [Int] ) (origin : Index) (target : Index) (amount : Int)
+      (origin_balance : Int) : List [Int] =
+    (get (accounts) (target) ) match  {
+      case Some (target_balance) =>
+        _transfer_with_balances (accounts) (origin) (target) (amount) (origin_balance) (target_balance)
+      case otherwise => accounts
+    }
+
   private def _transfer (accounts : List [Int] ) (origin : Index) (target : Index) (amount : Int)
       : List [Int] =
-    set (
-      set (accounts) (origin) ( (get (accounts) (origin) ) - amount)
-    ) (target) ( (get (accounts) (target) ) + amount)
+    (get (accounts) (origin) ) match  {
+      case Some (origin_balance) =>
+        _transfer_with (accounts) (origin) (target) (amount) (origin_balance)
+      case None => accounts
+    }
 
   private def _give (items : List [Item] ) (item_id : Index) (buyer : Index) (price : Int) : List [Item] =
     set (items) (item_id) (Item_ (buyer, price, false) )
@@ -103,7 +127,12 @@ trait MarketMod
     )
 
   def sell (market : Market) (item_id : Index) (buyer : Index) : Market =
-    _sell_item (market) (get (market .items) (item_id) ) (item_id) (buyer)
+    (get (market .items) (item_id) ) match  {
+      case Some (item) =>
+        _sell_item (market) (item) (item_id) (buyer)
+      case otherwise =>
+        market
+    }
 
 }
 
