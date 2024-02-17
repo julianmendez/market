7073d258f66c8a7ee880cb5883436e5f2a64ecb7
Julian Mendez <julian.mendez@gmail.com>
2024-02-17 21:29:23 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# This script translates the Soda source files into Scala and Lean.
+#
+# 2024-02-17
+
 
 pathToMain="core/src/main"
 pathToSodaFiles="${pathToMain}/scala/soda/se/umu/cs/soda/prototype/example/market"
 pathToLeanDir="${pathToMain}/lean/soda/se/umu/cs/soda/prototype/example/market"
 
-soda .
 
+# This translates all Soda files into Scala
+soda scala .
+
+
+# This translates all Soda files into Lean
 files=" \
   Basic \
   MyList \
@@ -16,5 +24,15 @@ files=" \
 for file in ${files}; do
   soda lean ${pathToSodaFiles}/${file}.soda ${pathToLeanDir}/${file}.lean
 done
+
+
+# This compiles a binary in Lean
+cd "${pathToMain}/lean"
+lake clean
+lake build
+
+
+# This is updates Lean
+elan update
 
 
