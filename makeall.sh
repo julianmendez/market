98519a84bc3548fc2682ab3af3d03d61f1b32210
Julian Mendez <julian.mendez@gmail.com>
2024-04-27 23:21:29 +0200

    Add script to compile the project

---
+++ makeall.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+#
+# This script builds the binary file.
+# It requires `sbt` [https://www.scala-sbt.org/].
+#
+
+sbt scalaVersion sbtVersion version clean compile test package assembly
+
+scalaVersion="3.4.0"
+binaryFile="market"
+executableStub="exec java -jar \$0 \"\$@\" ; exit"
+jarFile="target/scala-${scalaVersion}/${binaryFile}-*.jar"
+exampleFile="core/src/test/resources/example/example0.yaml"
+localExampleFile="example0.yaml"
+
+echo ${executableStub} >${binaryFile}
+cat ${jarFile} >>${binaryFile}
+chmod u+x ${binaryFile}
+
+if [ ! -f ${localExampleFile} ]; then
+  cp -p ${exampleFile} ${localExampleFile}
+fi
+
+
