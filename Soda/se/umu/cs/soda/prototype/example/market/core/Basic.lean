1899d8642f2c54cdd53c61575682d22a897cd997
Julian Mendez <julian.mendez@gmail.com>
2024-04-27 11:00:15 +0200

    Update definition of Nat

--- Soda/se/umu/cs/soda/prototype/example/market/core/Basic.lean
+++ Soda/se/umu/cs/soda/prototype/example/market/core/Basic.lean
@@ -1,5 +1,6 @@
 /-
 directive scala
+type Nat = Int
 object Succ_ {
   def unapply (n : Int) : Option [Int] =
     if (n <= 0) None else Some (n - 1)
@@ -7,13 +8,9 @@ object Succ_ {
 -/
 
 notation "Succ_" => Nat.succ
-notation "Int" => Nat
 
 /-
 directive coq
 Notation "head '+:' tail" := (cons (head) (tail) ) (at level 99).
 Notation "'Succ_'" := S (at level 99).
-Notation "'Int'" := nat (at level 99).
 -/
-
-notation "Nat" => Int
--- core/src/main/scala/soda/se/umu/cs/soda/prototype/example/market/core/Package.scala
+++ core/src/main/scala/soda/se/umu/cs/soda/prototype/example/market/core/Package.scala
@@ -5,6 +5,7 @@ package soda.se.umu.cs.soda.prototype.example.market.core
  *
  */
 
+type Nat = Int
 object Succ_ {
   def unapply (n : Int) : Option [Int] =
     if (n <= 0) None else Some (n - 1)
@@ -13,17 +14,13 @@ object Succ_ {
 /*
 directive lean
 notation "Succ_" => Nat.succ
-notation "Int" => Nat
 */
 
 /*
 directive coq
 Notation "head '+:' tail" := (cons (head) (tail) ) (at level 99) .
 Notation "'Succ_'" := S (at level 99) .
-Notation "'Int'" := nat (at level 99) .
 */
-
-type Nat = Int
 
 
 
